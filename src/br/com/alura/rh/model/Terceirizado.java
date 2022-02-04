4c5769cf8a926d9c4d02604f32cc32a7443f07c5
Josimar Minete <josimaminete@gmail.com>
2022-02-04 19:51:35 -0300

    Adiciona Composição no lugar de Herança.

--- src/br/com/alura/rh/model/Terceirizado.java
+++ src/br/com/alura/rh/model/Terceirizado.java
@@ -1,5 +1,17 @@
 package br.com.alura.rh.model;
 
+import java.math.BigDecimal;
+
 public class Terceirizado {
 
+	private DadosPessoais dadosPessoais;
+	private String empresa;
+	
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
 }
