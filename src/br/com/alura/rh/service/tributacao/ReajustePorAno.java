a061bf06290f5a7dfcdc0817f55ad0eabc5528fb
Josimar Minete <josimaminete@gmail.com>
2022-02-07 20:36:22 -0300

    Criando problema de sebregação de interface.

---
+++ src/br/com/alura/rh/service/tributacao/ReajustePorAno.java
@@ -0,0 +1,32 @@
+package br.com.alura.rh.service.tributacao;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ReajustePorAno implements Reajuste {
+
+	BigDecimal valor;
+	LocalDate data;
+
+	public ReajustePorAno(BigDecimal valor, LocalDate data) {
+		super();
+		this.valor = valor;
+		this.data = data;
+	}
+
+	@Override
+	public BigDecimal valor() {
+		return valor;
+	}
+
+	@Override
+	public LocalDate data() {
+		return data;
+	}
+
+	@Override
+	public BigDecimal valorImpostoRenda() {
+		return BigDecimal.ZERO;
+	}
+
+}
